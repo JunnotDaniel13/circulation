06afde8997aa60a5385b8e8dd67caccac1970d4e
Junnot Daniel <jerenasonjunnot@gmail.com>
2024-01-28 12:41:35 +0300

    Add a swagger documentation

--- index.ts
+++ index.ts
@@ -1,23 +1,26 @@
 //const express = require('express' )
 import express, { Express, Application, Request, Response } from "express";
-const app: Application = express();
-import { PrismaClient } from "@prisma/client";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerOutput from "./src/docs/swagger.json";
+import db from "./prisma/db";
+
+const app: Application = express();
 
 app.use(cors());
-const port = 3001;
-
-const prisma = new PrismaClient();
-// use `prisma` in your application to read and write data in your DB
+const port = process.env.PORT || 3000;
 
 app.get("/", async (req: Request, res: Response) => {
-  const cooperative = await prisma.cooperative.findMany();
+  const cooperative = await db.cooperative.findMany();
   res.json(cooperative);
 });
 
 app.get("/name", (req: Request, res: Response) => {
   res.send("Anarana : judi");
 });
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
